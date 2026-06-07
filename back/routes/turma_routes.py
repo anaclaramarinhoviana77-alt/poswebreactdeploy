0eec40f0c78709f683b0e946250a16af0f6c21e3
hiag-code <oliveirapradohiago@gmail.com>
2026-06-07 17:02:11 -0300

    v2

--- back/routes/turma_routes.py
+++ back/routes/turma_routes.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 
 from db.database import get_db
 from models.turma_model import Turma
+from models.disciplina_model import Disciplina
+from models.docente_model import Docente
 from schemas.turma_schema import TurmaCreate, TurmaResponse
 from core.security import get_current_user
 
@@ -10,6 +12,16 @@ router = APIRouter(prefix="/turmas", tags=["Turmas"])
 
 @router.post("/", response_model=TurmaResponse, status_code=status.HTTP_201_CREATED)
 def criar_turma(dados: TurmaCreate, db: Session = Depends(get_db), logado : dict = Depends(get_current_user)):
+
+    #verificar se o professor existe
+    professor_existe = db.query(Docente).filter(Docente.id == dados.docente_id).first()
+    if not professor_existe:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"professor com Dados {dados.docente_id}, não foi encontrado")
+    
+    #verificar se a disciplina existe
+    disciplina_existe = db.query(Disciplina).filter(Disciplina.id == dados.disciplina_id).first()
+    if not disciplina_existe:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"A disciplina com Dados {dados.disciplina_id}, não foi encontrado")
 
     #aqui checa se a disciplina ja tem turma
     if db.query(Turma).filter(Turma.disciplina_id == dados.disciplina_id).first():
